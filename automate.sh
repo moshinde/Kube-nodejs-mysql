cefbde8480967c389ad89f1c1d70eaed55a31458
moshinde <monicashinde3@gmail.com>
2018-10-22 17:03:43 +0200

    Successfully connected nodejs app deployment with mysql pod

---
+++ automate.sh
@@ -0,0 +1,5 @@
+kubectl create -f secrets.yml
+kubectl create -f mysql-compose.yml
+kubectl create -f mysql-service.yml
+kubectl create -f nodejs-mysql-deployment.yml
+kubectl create -f nodejs-service.yml
