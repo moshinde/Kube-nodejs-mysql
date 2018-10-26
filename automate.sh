9e60f117ba28f47532ef37d5106cccf041b756c3
moshinde <31102278+moshinde@users.noreply.github.com>
2018-10-26 13:55:41 +0200

    Automated the execution of yml files

--- automate.sh
+++ automate.sh
@@ -1,5 +1,8 @@
-kubectl create -f secrets.yml
-kubectl create -f mysql-compose.yml
-kubectl create -f mysql-service.yml
-kubectl create -f nodejs-mysql-deployment.yml
-kubectl create -f nodejs-service.yml
+#!/bin/sh
+kubectl create -f ./secrets.yml
+kubectl create -f ./mysql-compose.yml
+kubectl create -f ./mysql-service.yml
+kubectl create -f ./nodejs-mysql-deployment.yml
+kubectl create -f ./nodejs-service.yml
+kubectl get services
+minikube service spring-service --url
